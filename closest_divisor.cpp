4d5f43559655db9ed65fdaaa30e99172937f6d6f
Sh1ptoast <sh1ptoast@duck.com>
2025-10-03 15:00:13 -0400

    Update closest_divisor.cpp

--- closest_divisor.cpp
+++ closest_divisor.cpp
@@ -2,20 +2,26 @@
 #include <vector>
 using namespace std;
 
+// This code is meant to find the number (1-10) that's the closest to a divisor
+
 int main() {
-    int dividend_num;
-    int current_dividend;
+    int dividend_num; // number of dividends
     
     cout << "How many dividends do you want?\nNumber of dividends: ";
     cin >> dividend_num;
     
+    // storage for the dividends
     vector<double> dividend(dividend_num);
     
+    // filling the vector
     for (int i = 0; i < dividend_num; ++i) { // 0-3
         cout << "Dividend "<< (i + 1) << ": ";
-        cin >> current_dividend;
-        dividend[i] = current_dividend;
+        cin >> dividend[i];
     }
+    
+    // TODO: dividing by 1-dn, finding the closest divisor (average), outputting it and the quotient and the average remainder
+    
+    
     
     // just for testing
     for (int test = 0; test < dividend_num; ++test) {
