efb57aaa37ba4fe72273704ff9f45254a82f090b
Ship <sh1ptoast@duck.com>
2026-01-08 15:09:37 -0500

    Update calc.cpp
    changed RunState to case-based rather than if-else for simplicity

--- calc.cpp
+++ calc.cpp
@@ -4,146 +4,149 @@ using namespace std;
 enum class RunState { normal, invalid, modeExit, success };
 
 int main() {
-    int choice = 0;
-    int uses = 0;
-    RunState state = RunState::normal;
-    
-    while (true) {
-        
-        // handling errors and successes
-        if (state == RunState::invalid) {
-            cin.clear();
-            cin.ignore(1000,'\n');
-            choice = 0;
-            cout << "\nInvalid input, restarting now.\n\n";
-            state = RunState::normal;
-            continue;
-        } else if (state == RunState::modeExit) {
-            cin.clear();
-            cin.ignore(1000,'\n');
-            choice = 0;
-            cout << "\nExiting mode...\n\n";
-            state = RunState::normal;
-            continue;
-        } else if (state == RunState::success) {
-            ++uses;
-            choice = 0;
-            state = RunState::normal;
-            continue;
-        }
-        
-        // menu
-        if (choice == 0) {
-            if (uses == 0) {
-                cout << "Welcome to the calculator app! choose what you want to see. Type \"exit\" to exit a mode.\n" <<
-                "1. addition\n2. subtraction\n3. multiplication\n4. division (no remainder)\n5. factorials\n6. exit\n\n" <<
-                "Your choice: ";
-            } else {
-                cout << "Welcome back! choose what you want to see. Remember, you can type \"exit\" to exit a mode.\n" <<
-                "1. addition\n2. subtraction\n3. multiplication\n4. division (no remainder)\n5. factorials\n6. exit\n\n" <<
-                "Your choice: ";
-            }
-            cin >> choice;
-            
-            // error
-            if (cin.fail() || choice < 1 || choice > 6) {
-                state = RunState::invalid;
-                continue;
-            }
-            cout << "\n";
-        }
-        
-        // addition
-        else if (choice == 1) {
+	int choice = 0;
+	int uses = 0;
+	RunState state = RunState::normal;
 
-            int add1 = 0;
-            int add2 = 0;
-            int sum = 0;
-            
-            cout << "Type what two numbers you want to be added.\nFirst number: ";
-            cin >> add1;
-            // failsafe + exit
-            if (cin.fail()) {
-                state = RunState::modeExit;
-                continue;
-            }
-            cout << "\nAnd the second number: ";
-            cin >> add2;
-            // failsafe + exit
-            if (cin.fail()) {
-                state = RunState::modeExit;
-                continue;
-            }
-            
-            sum = add1 + add2;
-            cout << "The sum is "<< sum << ".\n\n";
-            state = RunState::success;
-            continue;
-        }
-        
-        // subtraction
-        else if (choice == 2) {
-            
-        }
-        
-        // multiplication
-        else if (choice == 3) {
-            
-        }
-        
-        // division (no remainder)
-        else if (choice == 4) {
-            
-        }
-        
-        // factorial
-        else if (choice == 5) {
-            int factorial = 1; // result
-            int fInput = 0; // input
-            
-            cout << "Type what number you want to be factorialed.\nFactorial: ";
-            cin >> fInput;
-            
-            // failsafe + exit
-            if (cin.fail()) {
-                state = RunState::modeExit;
-                continue;
-            }
-            
-            // if zero
-            else if (fInput == 0) {
-                cout << "The factorial is 1.\n\n";
-                state = RunState::success;
-                continue;
-            }
-            
-            // if <0
-            else if (fInput < 0) {
-                cout << "Error, less than zero.\n\n";
-                state = RunState::modeExit;
-                continue;
-            }
-            
-            // main factorial
-            else {
-                for (int i = fInput; i > 1; --i) { // does the math
-                    factorial *= i;
-                }
-                cout << "The factorial is " << factorial << ".\n\n";
-                state = RunState::success;
-                continue;
-            }
-        }
-        
-        // exit
-        else if (choice == 6) {
-            cout << "Byeeeeeee :3 :3 :3\n";
-            break;
-        }
-    }
-    
-    cout << "Number of uses: " << uses << "\n";
-    return 0;
+	while (true) {
+
+		// handling errors and successes
+		switch (state) {
+		case RunState::invalid:
+			cin.clear();
+			cin.ignore(1000,'\n');
+			choice = 0;
+			cout << "\nInvalid input, restarting now.\n\n";
+			state = RunState::normal;
+			continue;
+
+		case RunState::modeExit:
+			cin.clear();
+			cin.ignore(1000,'\n');
+			choice = 0;
+			cout << "\nExiting mode...\n\n";
+			state = RunState::normal;
+			continue;
+
+		case RunState::success:
+			++uses;
+			choice = 0;
+			state = RunState::normal;
+			continue;
+		}
+
+		// menu
+		if (choice == 0) {
+			if (uses == 0) {
+				cout << "Welcome to the calculator app! choose what you want to see. Type \"exit\" to exit a mode.\n" <<
+				     "1. addition\n2. subtraction\n3. multiplication\n4. division (no remainder)\n5. factorials\n6. exit\n\n" <<
+				     "Your choice: ";
+			} else {
+				cout << "Welcome back! choose what you want to see. Remember, you can type \"exit\" to exit a mode.\n" <<
+				     "1. addition\n2. subtraction\n3. multiplication\n4. division (no remainder)\n5. factorials\n6. exit\n\n" <<
+				     "Your choice: ";
+			}
+			cin >> choice;
+
+			// error
+			if (cin.fail() || choice < 1 || choice > 6) {
+				state = RunState::invalid;
+				continue;
+			}
+			cout << "\n";
+		}
+
+		// addition
+		else if (choice == 1) {
+
+			int add1 = 0;
+			int add2 = 0;
+			int sum = 0;
+
+			cout << "Type what two numbers you want to be added.\nFirst number: ";
+			cin >> add1;
+			// failsafe + exit
+			if (cin.fail()) {
+				state = RunState::modeExit;
+				continue;
+			}
+			cout << "\nAnd the second number: ";
+			cin >> add2;
+			// failsafe + exit
+			if (cin.fail()) {
+				state = RunState::modeExit;
+				continue;
+			}
+
+			sum = add1 + add2;
+			cout << "The sum is "<< sum << ".\n\n";
+			state = RunState::success;
+			continue;
+		}
+
+		// subtraction
+		else if (choice == 2) {
+
+		}
+
+		// multiplication
+		else if (choice == 3) {
+
+		}
+
+		// division (no remainder)
+		else if (choice == 4) {
+
+		}
+
+		// factorial
+		else if (choice == 5) {
+			int factorial = 1; // result
+			int fInput = 0; // input
+
+			cout << "Type what number you want to be factorialed.\nFactorial: ";
+			cin >> fInput;
+
+			// failsafe + exit
+			if (cin.fail()) {
+				state = RunState::modeExit;
+				continue;
+			}
+
+			// if zero
+			else if (fInput == 0) {
+				cout << "The factorial is 1.\n\n";
+				state = RunState::success;
+				continue;
+			}
+
+			// if <0
+			else if (fInput < 0) {
+				cout << "Error, less than zero.\n\n";
+				state = RunState::modeExit;
+				continue;
+			}
+
+			// main factorial
+			else {
+				for (int i = fInput; i > 1; --i) { // does the math
+					factorial *= i;
+				}
+				cout << "The factorial is " << factorial << ".\n\n";
+				state = RunState::success;
+				continue;
+			}
+		}
+
+		// exit
+		else if (choice == 6) {
+			cout << "Byeeeeeee :3 :3 :3\n";
+			break;
+		}
+	}
+
+	cout << "Number of uses: " << uses << "\n";
+	return 0;
 }
 
 /*
